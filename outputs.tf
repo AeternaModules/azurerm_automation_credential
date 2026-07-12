e4bd3198aaa5bfa605daa0e1d04b155599eafe77
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,26 @@
-output "automation_credentials" {
-  description = "All automation_credential resources"
-  value       = azurerm_automation_credential.automation_credentials
-  sensitive   = true
-}
 output "automation_credentials_automation_account_name" {
-  description = "List of automation_account_name values across all automation_credentials"
-  value       = [for k, v in azurerm_automation_credential.automation_credentials : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_credentials, keyed the same as var.automation_credentials"
+  value       = { for k, v in azurerm_automation_credential.automation_credentials : k => v.automation_account_name }
 }
 output "automation_credentials_description" {
-  description = "List of description values across all automation_credentials"
-  value       = [for k, v in azurerm_automation_credential.automation_credentials : v.description]
+  description = "Map of description values across all automation_credentials, keyed the same as var.automation_credentials"
+  value       = { for k, v in azurerm_automation_credential.automation_credentials : k => v.description }
 }
 output "automation_credentials_name" {
-  description = "List of name values across all automation_credentials"
-  value       = [for k, v in azurerm_automation_credential.automation_credentials : v.name]
+  description = "Map of name values across all automation_credentials, keyed the same as var.automation_credentials"
+  value       = { for k, v in azurerm_automation_credential.automation_credentials : k => v.name }
 }
 output "automation_credentials_password" {
-  description = "List of password values across all automation_credentials"
-  value       = [for k, v in azurerm_automation_credential.automation_credentials : v.password]
+  description = "Map of password values across all automation_credentials, keyed the same as var.automation_credentials"
+  value       = { for k, v in azurerm_automation_credential.automation_credentials : k => v.password }
   sensitive   = true
 }
 output "automation_credentials_resource_group_name" {
-  description = "List of resource_group_name values across all automation_credentials"
-  value       = [for k, v in azurerm_automation_credential.automation_credentials : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_credentials, keyed the same as var.automation_credentials"
+  value       = { for k, v in azurerm_automation_credential.automation_credentials : k => v.resource_group_name }
 }
 output "automation_credentials_username" {
-  description = "List of username values across all automation_credentials"
-  value       = [for k, v in azurerm_automation_credential.automation_credentials : v.username]
+  description = "Map of username values across all automation_credentials, keyed the same as var.automation_credentials"
+  value       = { for k, v in azurerm_automation_credential.automation_credentials : k => v.username }
 }
 
